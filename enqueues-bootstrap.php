f6e7b6e138f8fe3524c41f90181cbddf40827a01
Anthony Thorne <athorne@thecode.co>
2024-10-25 08:08:14 +1000

    Optimize Enqueues bootstrap file for duplicate load prevention and remove the check for local env, as that file with that function would not be loaded

--- enqueues-bootstrap.php
+++ enqueues-bootstrap.php
@@ -10,46 +10,50 @@
 
 namespace Enqueues;
 
+// Prevent duplicate loading if this package is already loaded.
+if ( defined( 'Enqueues\\APP_NAME' ) ) {
+	return;
+}
+
 /**
- * Load package, first lets check if this is a standalone and then check if this is a dependency.
+ * Load the package: check if this is a standalone or dependency setup.
  */
 $autoload_file = __DIR__ . '/vendor/autoload.php';
 
-// Check if the file exists. If not, check the parent directory as this may be a dependency.
+// Check if autoload file exists. If not, check parent directory as it may be a dependency.
 if ( ! file_exists( $autoload_file ) ) {
 	$autoload_file = dirname( dirname( __DIR__ ) ) . '/autoload.php';
 }
 
-if ( file_exists( $autoload_file ) ) {
-
-	require_once $autoload_file;
-
-	if ( ! defined( '\\Enqueues\\APP_NAME' ) ) {
-		define( 'Enqueues\\APP_NAME', basename( __FILE__, '.php' ) );
-	}
-
-	$enqueues_app = new \Enqueues\Base\Main\Application(
-		APP_NAME,
-		__DIR__,
-		[
-			new \Enqueues\Controller\ThemeEnqueueMainController(),
-			new \Enqueues\Controller\ThemeEnqueueJqueryController(),
-			new \Enqueues\Controller\ThemeInlineAssetController(),
-			new \Enqueues\Controller\BlockEditorRegistrationController(),
-		]
-	);
-
-	global $enqueues_app_config;
-
-	/**
-	 * Variable Type Definition.
-	 *
-	 * @param \Enqueues\Base\Library\Config $enqueues_app_config The config for this app.
-	 */
-	$enqueues_app_config = $enqueues_app->get_config();
-
-} elseif ( function_exists( '\\Enqueues\\is_local' ) && \Enqueues\is_local() ) {
-
-	// Trigger an error for local environment only.
-	wp_die( 'Run composer for the "enqueues" MU Plugin.', E_USER_WARNING ); // phpcs:ignore
+if ( ! file_exists( $autoload_file ) ) {
+	return;
 }
+
+require_once $autoload_file;
+
+// Define the APP_NAME constant to prevent further duplicate loads.
+if ( ! defined( 'Enqueues\\APP_NAME' ) ) {
+	define( 'Enqueues\\APP_NAME', basename( __FILE__, '.php' ) );
+}
+
+// Initialize the Enqueues application.
+$enqueues_app = new \Enqueues\Base\Main\Application(
+	APP_NAME,
+	__DIR__,
+	[
+		new \Enqueues\Controller\ThemeEnqueueMainController(),
+		new \Enqueues\Controller\ThemeEnqueueJqueryController(),
+		new \Enqueues\Controller\ThemeInlineAssetController(),
+		new \Enqueues\Controller\BlockEditorRegistrationController(),
+	]
+);
+
+// Make application configuration globally available.
+global $enqueues_app_config;
+
+/**
+ * Variable Type Definition.
+ *
+ * @var \Enqueues\Base\Library\Config $enqueues_app_config The configuration for this app.
+ */
+$enqueues_app_config = $enqueues_app->get_config();
